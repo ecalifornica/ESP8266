80acf9e65bd765087a93522b30f4d72404dfe661
Robert Queenin <queenin@gmail.com>
2016-08-28 02:58:20 -0400

    testing Sentry

--- api_stub/api_stub.py
+++ api_stub/api_stub.py
@@ -1,3 +1,4 @@
+import logging
 import time
 
 from bokeh.plotting import figure, output_file, save
@@ -5,92 +6,104 @@ from bokeh.models import LinearAxis, Range1d
 from flask import Flask, request, send_from_directory
 import pandas as pd
 import pymongo
+from raven.contrib.flask import Sentry
 
 import config
 
+app = Flask(__name__)
+
+sentry = Sentry(app, logging=True, level=logging.ERROR,
+                dsn=config.sentry_dsn)
+
 mc = pymongo.MongoClient()
 data_store = mc.sensornet.sensors
-
-app = Flask(__name__)
 
 
 def produce_plot(mean_window=1):
     '''
     '''
 
-    timestamps, moistures, voltages = [], {'moisture': []}, {'voltage': []}
-    for data_point in data_store.find():
+    try:
+        timestamps, moistures, voltages = [], {'moisture': []}, {'voltage': []}
+        for data_point in data_store.find():
 
-        moisture = float(data_point.get('soil moisture', 0))
-        moistures['moisture'].append(moisture)
-        avg_moist_df = pd.DataFrame(moistures)
-        avg_moist = avg_moist_df.rolling(center=False,
-                                         window=mean_window).mean()
-
-        voltage = float(data_point.get('voltage', 0))
-        voltages['voltage'].append(voltage)
-        avg_voltage_df = pd.DataFrame(voltages)
-        avg_voltage = avg_voltage_df.rolling(center=False,
+            moisture = float(data_point.get('soil moisture', 0))
+            moistures['moisture'].append(moisture)
+            avg_moist_df = pd.DataFrame(moistures)
+            avg_moist = avg_moist_df.rolling(center=False,
                                              window=mean_window).mean()
 
-        # UTC -> PDT, server is 3 hours fast
-        timestamp = data_point.get('timestamp') - 43200
-        timestamps.append(timestamp * 1000)
+            voltage = float(data_point.get('voltage', 0))
+            voltages['voltage'].append(voltage)
+            avg_voltage_df = pd.DataFrame(voltages)
+            avg_voltage = avg_voltage_df.rolling(center=False,
+                                                 window=mean_window).mean()
 
-    output_file(config.bokeh_output_dir + 'lines.html')
-    p = figure(title='ESP8266 Sensor #1',
-               x_axis_label='time (UTC-8:00)',
-               x_axis_type='datetime',
-               y_axis_label='voltage',
-               tools='',
-               y_range=(3,4))
+            # UTC -> PDT, server is 3 hours fast
+            timestamp = data_point.get('timestamp') - 43200
+            timestamps.append(timestamp * 1000)
 
-    p.line(timestamps,
-           avg_voltage,
-           legend="voltage",
-           line_width=2,
-           color='red')
+        output_file(config.bokeh_output_dir + 'lines.html')
+        p = figure(title='ESP8266 Sensor #1',
+                   x_axis_label='time (UTC-8:00)',
+                   x_axis_type='datetime',
+                   y_axis_label='voltage',
+                   tools='',
+                   y_range=(3, 4.3))
 
-    # moisture
-    p.extra_y_ranges = {'moisture': Range1d(start=-5, end=100)}
-    p.line(timestamps,
-           avg_moist,
-           legend="moisture",
-           line_width=2,
-           color='blue',
-           y_range_name='moisture')
-    p.add_layout(LinearAxis(y_range_name='moisture',
-                            axis_label='moisture'),
-                 'right')
+        p.line(timestamps,
+               avg_voltage,
+               legend="voltage",
+               line_width=2,
+               color='red')
 
-    save(p)
+        # moisture
+        p.extra_y_ranges = {'moisture': Range1d(start=-5, end=100)}
+        p.line(timestamps,
+               avg_moist,
+               legend="moisture",
+               line_width=2,
+               color='blue',
+               y_range_name='moisture')
+        p.add_layout(LinearAxis(y_range_name='moisture',
+                                axis_label='moisture'),
+                     'right')
+
+        save(p)
+    except:
+        sentry.captureMessage('produce plot failure')
+        sentry.captureException()
 
 
 @app.route('/', methods=['GET', 'POST'])
 def barf():
 
     if request.method == 'POST':
-        print('\n\nPOST')
-        # print(request.data)
-        soil_moisture = float(request.form['soil_moisture'])
-        voltage = float(request.form['bus_voltage'])
-        timestamp = time.gmtime()
-        data_point = {'soil moisture': soil_moisture,
-                      'voltage': voltage,
-                      'serial_number': 1,
-                      'timestamp': time.mktime(timestamp)}
-        print('{} {}% {}v'.format(time.strftime('%Y-%m-%d %H:%M:%S',
-                                                timestamp),
-                                  soil_moisture,
-                                  voltage))
+        try:
+            print('\n\nPOST')
+            # print(request.data)
+            soil_moisture = float(request.form['soil_moisture'])
+            voltage = float(request.form['bus_voltage'])
+            timestamp = time.gmtime()
+            data_point = {'soil moisture': soil_moisture,
+                          'voltage': voltage,
+                          'serial_number': 1,
+                          'timestamp': time.mktime(timestamp)}
+            print('{} {}% {}v'.format(time.strftime('%Y-%m-%d %H:%M:%S',
+                                                    timestamp),
+                                      soil_moisture,
+                                      voltage))
 
-        r = data_store.insert_one(data_point)
-        print('MONGO ID:', r.inserted_id)
-        
-        produce_plot()
-        
-        # TODO: return sensor data hash
-        return str(data_point['timestamp'])
+            r = data_store.insert_one(data_point)
+            print('MONGO ID:', r.inserted_id)
+
+            produce_plot()
+
+            # TODO: return sensor data hash
+            return str(data_point['timestamp'])
+        except:
+            sentry.captureMessage('sensor data POST failure')
+            sentry.captureException()
 
     elif request.method == 'GET':
         print('\n\nGET')
@@ -124,13 +137,13 @@ def gap_test():
 
     inc = df.close > df.open
     dec = df.open > df.close
-    w = 12*60*60*1000 # half day in ms
+    w = 12*60*60*1000  # half day in ms
 
     TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
 
-    p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=1000, title = "MSFT Candlestick")
+    p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=1000, title="MSFT Candlestick")
     p.xaxis.major_label_orientation = pi/4
-    p.grid.grid_line_alpha=0.3
+    p.grid.grid_line_alpha = 0.3
 
     p.segment(df.date, df.high, df.date, df.low, color="black")
     p.rect(df.date[inc], mids[inc], w, spans[inc], fill_color="#D5E1DD", line_color="black")
--- api_stub/example_config.py
+++ api_stub/example_config.py
@@ -1,3 +1,4 @@
 flask_port = 8080
 static_files_path = '/var/www/sensornet'
 bokeh_output_dir = 'api_stub/bokeh_output/'
+sentry_dsn = 'https://KEY@app.getsentry.com/ENDPOINT'
